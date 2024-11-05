7813f1627ce2d7f9f165ece6e95f504de2591a13
Han-ChangJun <hci8@naver.com>
2024-11-05 10:44:12 +0900

    start

--- express/index.js
+++ express/index.js
@@ -11,6 +11,11 @@ app.get("/", () => {
 app.get("/hello", () => {
   console.log("hello 주소요청");
 });
+app.get("/qs", (req) => {
+  console.log(req.query);
+  const { p1, p2 } = req.query;
+  console.log(`p1: ${p1},p2:${p2}`);
+});
 
 //board/10 , product/6
 app.get("/:id", (req) => {
