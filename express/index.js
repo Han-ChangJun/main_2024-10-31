e59221a8f914292e6a3eebd3fb476f78156dae83
Han-ChangJun <hci8@naver.com>
2024-11-05 10:39:05 +0900

    path방식

---
+++ express/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+app.listen(8000, () => {
+  console.log("서버시작!");
+});
+app.get("/", () => {
+  console.log("기본주소 요청");
+});
+//hello get 요청이 오면 콘솔에 hello 주소 요청 출력
+app.get("/hello", () => {
+  console.log("hello 주소요청");
+});
+
+//board/10 , product/6
+app.get("/:id", (req) => {
+  console.log(req.params);
+  const { id } = req.params;
+  console.log(id);
+});
